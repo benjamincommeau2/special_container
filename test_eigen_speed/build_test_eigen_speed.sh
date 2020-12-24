31fefe00a8896fd14c50160376a1e2adb106f979
Benjamin Commeau <benjamincommeau2@gmail.com>
2020-12-24 12:56:43 -0700

    Made new folder test_eigen_speed/ to test Eigen sparse matrix multiplication speed.

--- test_eigen_speed/build_test_eigen_speed.sh
+++ test_eigen_speed/build_test_eigen_speed.sh
@@ -1,6 +1,6 @@
 opt="O3" # removes performance timing loops, compliant high optimization
 opt="O1" # allows performance timing loops
-script=testMap
+script=test_eigen
 > compile.errors.txt
 g++ -std=c++20 -$opt -c $script.cpp 2>&1 | tee -a compile.errors.txt
 g++ -std=c++20 -$opt -o myexec.o $script.o 2>&1 | tee -a compile.errors.txt
